07a46da167619e2a2fac5d3e73884ecdfaa0a542
Snathad <94551808+Snathad@users.noreply.github.com>
2022-03-09 15:35:38 +0000

    Add files via upload

--- task_3_sans_timing.ino
+++ task_3_sans_timing.ino
@@ -7,21 +7,24 @@
 //length of a single pulse would be counted and scaled up based on its duration//
 
 //establish frequency input pin as a variable//
-
-  }
-}
-void setup() {
+ const int freq_in = 33;
+ int freq_flag = 0;
+ int frequency = 0;
  
-  const int freq_in = 33;
-  const int freq_flag = 0;
-  pinMode (freq_in, INPUT);
+
+void setup() {
+
+ 
+  pinMode (freq_in, INPUT); 
+  Serial.begin(9600);
 }
 //this subfunction creates a delay based on the clock in the time library. A traditional delay was found to pause the whole code and prevent the button inputs form registering//
 void delay2(int nosecs){
   clock_t start_time= clock(); //define the start time of the clock
   while (clock() < start_time + nosecs);  //adds the number of elapsed milliseconds on to the clock so timing can be calculated
+}
 
-void freq_measure (int frequency=0){
+void freq_measure (int frequency=0, const int freq_in=33, int freq_flag=0){
   if (freq_in==LOW){
     freq_flag=1;
   }
@@ -29,12 +32,14 @@ void freq_measure (int frequency=0){
     delay (0.5);
     freq_flag=0;}
 
-clock_t start_timeF= clock(); //define the start time of the clock
-  while (clock() < start_timeF + 40);{  //adds the number of elapsed milliseconds on to the clock so timing can be calculated
-    if (freq_flag==1 && freq_in==HIGH){ 
-      frequency=frequency+1;
-      
+  clock_t start_timeF= clock(); //define the start time of the clock
+    while (clock() < start_timeF + 40);{  //adds the number of elapsed milliseconds on to the clock so timing can be calculated
+      if (freq_flag==1 && freq_in==HIGH){ 
+        frequency=frequency+1;
+        }
+    }
+}
 void loop() {
-  
-
+  freq_measure();
+Serial.print(frequency);
 }
