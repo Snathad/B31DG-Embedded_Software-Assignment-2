d0a64f293054b4127a0c8f21245eb908517361a4
Snathad <94551808+Snathad@users.noreply.github.com>
2022-03-10 16:44:41 +0000

    Add files via upload

--- Tasks_4_5_7_8_sans_timing.ino
+++ Tasks_4_5_7_8_sans_timing.ino
@@ -9,34 +9,42 @@
 //Establish analogue read as a variable//
 const int analogue_in = 35;
 const int red_led = 15;
-float analogue_hist[4]={0,0,0,0};
-float analogue_average = 0;
+float analogue_hist0;
+float analogue_hist1;
+float analogue_hist2;
+float analogue_hist3;
+float analogue_average ;
 const float analogue_max = 3.3;
 int error_code = 0;
 void setup() {
   pinMode(red_led, OUTPUT);
   pinMode(analogue_in, INPUT);
   Serial.begin(9600);
+  analogue_hist0=analogRead(analogue_in);
+  analogue_hist1=analogue_hist0;
+  analogue_hist2=analogue_hist1;
+  analogue_hist3=analogue_hist2;
 }
 
 void analogue_process(){
-  analogue_hist[3]=analogue_hist[2];
-  analogue_hist[2]=analogue_hist[1];
-  analogue_hist[1]=analogue_hist[0];
-  analogue_hist[0]=analogRead(analogue_in);
+  analogue_hist3=analogue_hist2;
+  analogue_hist2=analogue_hist1;
+  analogue_hist1=analogue_hist0;
+  analogue_hist0=analogRead(analogue_in)*(3.3/4095);
 }
 
 void analogue_filter(){
-  analogue_average= (analogue_hist[0]+analogue_hist[1]+analogue_hist[2]+analogue_hist[3])/4;
-  Serial.println("filtered analog value");
-  Serial.print(analogue_average);
+  analogue_average= (analogue_hist0+analogue_hist1+analogue_hist2+analogue_hist3)/4;
+  Serial.print("filtered analog value");
+  Serial.println(analogue_average);
+  Serial.print(analogue_hist0);
 }
 
 void analogue_error(){
-  if (analogue_hist[0] > (0.5*analogue_max)){
+  if (analogue_hist0 > (0.5*analogue_max)){
     error_code = 1;
     }
-    else if (analogue_hist[0] < (0.5*analogue_max)){
+    else if (analogue_hist0 < (0.5*analogue_max)){
     error_code = 0;  
     }
 }
